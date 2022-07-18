0c2158b7d03b9598e7650b8e58c0317f0d29c4b4
LucianoAmato <luciano2697@outlook.com.ar>
2022-07-17 21:55:38 -0300

    arrays

--- javascript/main.js
+++ javascript/main.js
@@ -1,141 +1,182 @@
 // Carrito de compra.
 
-//Menú comienzo: (actualmente fuera de servicio)
+// REGISTRO DE USUARIOS comienzo
+let registroUsuarios = [{usuario: "luciano", fecha: "17/7/2022, 02:26:43"}];
 
-// class Producto {
-//     constructor(nombre, categoria, precio){
-//         this.nombre = nombre
-//         this.categoria = categoria
-//         this.precio = parseInt(precio)
-//     }
-// }
+const fechaActual = new Date();
 
-// const simple = new Producto("simple", "hamburguesa", 280)
-// const doble = new Producto ("doble", "hamburguesa", 350)
-// const tinyDream = new Producto ("tinyDream", "hamburguesa", 270)
-// const extraCheese = new Producto ("extraCheese", "hamburguesa", 350)
-// const completa = new Producto ("completa", "hamburguesa", 500)
-// const bigDream = new Producto ("bigDream", "hamburguesa", 430)
-// const dreamChoise = new Producto ("dreamChoise", "hamburguesa", 580)
-// const theChicken = new Producto ("theChicken", "hamburguesa", 270)
-// const veganDream = new Producto ("veganDream", "hamburguesa", 380)
+let nombreUsuario = prompt("Ingrese su nombre").toLowerCase();
 
-// const papasFritas = new Producto ("papasFritas", "acompaniamiento", 120)
-// const ensalada = new Producto ("ensalada", "acompaniamiento", 200)
+UsuarioNuevo()
 
-// const cocaCola = new Producto ("cocaCola", "bebida", 130)
-// const cocaColaZero = new Producto ("cocaColaZero", "bebida", 130)
-// const agua = new Producto ("agua", "bebida", 80)
-
-//Menú fin: (actualmente fuera de servicio)
-
-// Hoy en dia rigen los combos, proximamente regiran los productos.
-
-// Precios de Combos:
-class Combos {
-    constructor(categoria, precio){
-        this.categoria = categoria
-        this.precio = parseInt(precio)
-    }
-}
-
-const comboA = new Combos ("A", 550)
-const comboB = new Combos ("B", 350)
-const comboC = new Combos ("C", 250)
-// Precios de Combos:
-
-
-// Costo de envio:
-class Envio {
-    constructor(zona, costo){
-        this.zona = zona
-        this.costo = parseInt(costo)
-    }
-}
-
-const zonaA = new Envio ("A", 200)
-const zonaB = new Envio ("B", 300)
-const zonaC = new Envio ("C", 400)
-// A: palermo.
-// B: belgrano, colegiales, chacarita, villa crezpo, almagro, recoleta.
-// C: nuniez, coghlan, villa ortuzar, paternal, caballito, balvanera.
-// Costo de envio:
-
-
-let combo = prompt("Elige tu combo...\nDisponibles:\nA - Hamburguesa, papas fritas y CocaCola.\nB - Hamburguesa y papas fritas.\nC - Hamburguesa.").toUpperCase();
-
-while (combo != ""){
-    if (combo == "A") {
-
-        const precioCombo = comboA.precio;
-        console.log(`Combo A: $${precioCombo}`);
-        alert(`El total a abonar del combo ${combo}, es de $${PrecioTotalCombo(precioCombo)}`) 
-        break;
-
-    } else if (combo == "B") {
-
-        const precioCombo = comboB.precio;
-        console.log(`Combo B: $${precioCombo}`);
-        alert(`El total a abonar del combo ${combo}, es de $${PrecioTotalCombo(precioCombo)}`) 
-        break;
-
-    } else if (combo == "C") {
-
-        const precioCombo = comboC.precio;
-        console.log(`Combo C: $${precioCombo}`);
-        alert(`El total a abonar del combo ${combo}, es de $${PrecioTotalCombo(precioCombo)}`)
-        break;
-        
-    } else {
-        alert("Insete una opción valida")
-    }
-}
-
-
-let envio = prompt("Elige tu zona\nA - Palermo.\nB - belgrano, colegiales, chacarita, villa crezpo, almagro, recoleta.\nC - nuñez, coghlan, villa ortuzar, paternal, caballito, balvanera.\nD - Retiro en local.").toLocaleUpperCase();
-
-while (envio != "") { 
-
-    if (envio == "A") {
-
-        const precioEnvio = zonaA.costo;
-        console.log(`Envio A: $${precioEnvio}`);
-        alert(`El total a abonar de envio es de $${PrecioTotalEnvio(precioEnvio)}`);
-        break;
-
-    } else if (envio == "B") {
-
-        const precioEnvio = zonaB.costo;
-        console.log(`Envio A: $${precioEnvio}`);
-        alert(`El total a abonar de envio es de $${PrecioTotalEnvio(precioEnvio)}`);
-        break;
-
-    } else if (envio == "C") {
-
-        const precioEnvio = zonaC.costo;
-        console.log(`Envio A: $${precioEnvio}`);
-        alert(`El total a abonar de envio es de $${PrecioTotalEnvio(precioEnvio)}`);
-        break;
-
-    } else if (envio == "D") {
-
-        alert("Usted retirara el pedido en el local.")
-        break;
-
-    } else {
-    alert("Inserte una opción valida.");
-    }
+function UsuarioNuevo() {
     
-}
+    do { 
+        if (nombreUsuario == ""){
 
-alert("Muchas gracias por su compra.\nLe llegara un mail con la confirmación del pedido.")
+            alert("Por favor, ingrese un nombre valido.")
+            nombreUsuario = prompt("Ingrese su nombre").toLowerCase();
 
-function PrecioTotalCombo (precio) {
-    const precioTotalCombo = precio;
-    return precioTotalCombo;
-}
+        }
 
-function PrecioTotalEnvio (precio) {
-    const precioTotalEnvio = precio;
-    return precioTotalEnvio;
+    }while (nombreUsuario == "") { 
+
+        for(let u in registroUsuarios)
+        
+        if (registroUsuarios[u].usuario.includes(nombreUsuario) == false) {
+
+            console.log(`${nombreUsuario} ingreso al sitio`);
+            console.log(`Esta registrado? ${registroUsuarios.includes(nombreUsuario)}`);
+            registroUsuarios.push({usuario: nombreUsuario , fecha: (fechaActual.toLocaleString())});
+            console.log(`${nombreUsuario} se ha ingresado al sistema.`);
+
+        } else if (registroUsuarios[u].usuario.includes(nombreUsuario) == true) {
+            console.log(`${nombreUsuario} ya ha ingresado con anterioridad.`)
+        }
+    }    
 }
+console.log(registroUsuarios);
+// REGISTRO DE USUARIOS fin
+
+
+// SECCION DE COMPRA comienzo
+let productos = [
+    {nombre: "simple", caterogia: "hamburguesa", precio: 280},
+    {nombre: "doble", caterogia: "hamburguesa", precio: 350},
+    {nombre: "tinyDream", caterogia: "hamburguesa", precio: 270},
+    {nombre: "extraCheese", caterogia: "hamburguesa", precio: 350},
+    {nombre: "completa", caterogia: "hamburguesa", precio: 500},
+    {nombre: "bigDream", caterogia: "hamburguesa", precio: 430},
+    {nombre: "dreamChoise", caterogia: "hamburguesa", precio: 580},
+    {nombre: "theChicken", caterogia: "hamburguesa", precio: 270},
+    {nombre: "veganDream", caterogia: "hamburguesa", precio: 380},
+    {nombre: "papasFritas", caterogia: "acompaniamiento", precio: 120},
+    {nombre: "ensalada", caterogia: "acompaniamiento", precio: 200},
+    {nombre: "cocaCola", caterogia: "bebida", precio: 130},
+    {nombre: "cocaColaZero", caterogia: "bebida", precio: 130},
+    {nombre: "agua", caterogia: "bebida", precio: 80}
+];
+
+// Los envios quedan temporalmente fuera de servicio.
+// let envio = [
+//     {zona: "A", localidad: "palermo.",precio: 200},
+//     {zona: "B", localidad: "belgrano, colegiales, chacarita, villa crezpo, almagro, recoleta.",precio: 300},
+//     {zona: "C", localidad: "nuniez, coghlan, villa ortuzar, paternal, caballito, balvanera.",precio: 400}
+// ];
+
+
+let productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+let carrito = [];
+
+let totalCarrito = 0;
+
+CompraCarrito()
+
+function CompraCarrito() {
+
+    do { 
+        if ((productoSeleccionado != "") && (productoSeleccionado == "A")){
+
+            carrito.push(productos[0]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[0].nombre}, de $${productos[0].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "B")) {
+
+            carrito.push(productos[1]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[1].nombre}, de $${productos[1].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "C")) {
+
+            carrito.push(productos[2]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[2].nombre}, de $${productos[2].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "D")) {
+
+            carrito.push(productos[3]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[3].nombre}, de $${productos[3].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "E")) {
+
+            carrito.push(productos[4]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[4].nombre}, de $${productos[4].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "F")) {
+
+            carrito.push(productos[5]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[5].nombre}, de $${productos[5].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "G")) {
+
+            carrito.push(productos[6]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[6].nombre}, de $${productos[6].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "H")) {
+
+            carrito.push(productos[7]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[7].nombre}, de $${productos[7].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "I")) {
+
+            carrito.push(productos[8]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[8].nombre}, de $${productos[8].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "J")) {
+
+            carrito.push(productos[9]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[9].nombre}, de $${productos[9].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "K")) {
+
+            carrito.push(productos[10]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[10].nombre}, de $${productos[10].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "L")) {
+
+            carrito.push(productos[11]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[11].nombre}, de $${productos[11].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "M")) {
+
+            carrito.push(productos[12]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[12].nombre}, de $${productos[12].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else if ((productoSeleccionado != "") && (productoSeleccionado == "N")) {
+
+            carrito.push(productos[13]);
+            console.log(`${nombreUsuario} agrego el producto: ${productos[13].nombre}, de $${productos[13].precio} al carrito.`)
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        } else {
+
+            alert("Inserte una opción valida.")
+            productoSeleccionado = prompt("Seleccione los productos que quiera:\nUno a la vez.\nAl finalizar con la seleccion ingrese X .\nHAMBURGUESAS:\nA - Simple $280\nB - Doble $350\nC - TinyDream $270\nD - ExtraCheese $350\nE - Completa $500\nF - BigDream $430\nG - DreamChoise $580\nH - TheChicken $270\nI - VeganDream $380\nACOMPANIAMIENTOS:\nJ - PapasFritas $120\nK - Ensalada $200\nBEBIDAS:\nL - CocaCola $130\nM - CocaColaZero $130\nN - Agua $80\nX - para terminar la selección.").toLocaleUpperCase();
+
+        }
+
+    } while (productoSeleccionado != "X") {
+
+        carrito.forEach(producto => {
+            totalCarrito += producto.precio;
+        })
+        console.log(`El total de la compra de ${nombreUsuario}, es de: $${totalCarrito}, realizada a las ${fechaActual.toTimeString()}`);
+        alert(`Su total a abonar es de ${totalCarrito}`);
+    }
+    return totalCarrito;
+}  
+  
+console.log(carrito)
+// SECCION DE COMPRA fin
